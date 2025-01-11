c4550e4ed20c804f6d306bdc4b095da5eaf30de1
mahade <mahade.adib45@gmail.com>
2025-01-11 19:52:26 +0600

    error solving

--- client/src/components/common/star-rating.jsx
+++ client/src/components/common/star-rating.jsx
@@ -5,20 +5,18 @@ function StarRatingComponent({ rating, handleRatingChange }) {
   console.log(rating, "rating");
 
   return [1, 2, 3, 4, 5].map((star) => (
-    <Button
-      className={`p-2 rounded-full transition-colors ${
-        star <= rating
+    <Button key={"random"}
+      className={`p-2 rounded-full transition-colors ${star <= rating
           ? "text-yellow-500 hover:bg-black"
           : "text-black hover:bg-primary hover:text-primary-foreground"
-      }`}
+        }`}
       variant="outline"
       size="icon"
       onClick={handleRatingChange ? () => handleRatingChange(star) : null}
     >
       <StarIcon
-        className={`w-6 h-6 ${
-          star <= rating ? "fill-yellow-500" : "fill-black"
-        }`}
+        className={`w-6 h-6 ${star <= rating ? "fill-yellow-500" : "fill-black"
+          }`}
       />
     </Button>
   ));
